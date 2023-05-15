d3a9cc484d7424315c2fd2fafe0bf7fae29e497f
v1c7or20 <victor.pena@utec.edu.pe>
2023-05-14 21:08:57 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,13 +13,13 @@ const char *vertexShaderSource = "#version 330 core\n"
                                  "layout (location = 0) in vec3 aPos;\n"
                                  "void main()\n"
                                  "{\n"
-                                 "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+                                 "   gl_Position = vec4(aPos, 1.0);\n"
                                  "}\0";
 const char *fragmentShaderSource = "#version 330 core\n"
                                    "out vec4 FragColor;\n"
                                    "void main()\n"
                                    "{\n"
-                                   "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+                                   "   FragColor = vec4(1.0f, 0.0f, 0.0f, 1.0f);\n"
                                    "}\n\0";
 
 int main()
@@ -99,13 +99,20 @@ int main()
     // set up vertex data (and buffer(s)) and configure vertex attributes
     // ------------------------------------------------------------------
     float vertices[] = {
-            -0.5f, -0.5f, 0.0f, // left
-            0.5f, -0.5f, 0.0f, // right
-            0.0f,  0.5f, 0.f,  // top
-            1,-1,0,
-            1,1,0,
-            0.5,1, 0
+            0, 0, 0,
+            0.25, 0, 0.5,
+            0.5, 0, 0,
+            0.25, 0, 0.5,
+            0.5, 0, 0,
+            0.25, 0.5, 0,
+            0, 0, 0,
+            0.25, 0, 0.5,
+            0.25, 0.5, 0,
+            0, 0, 0,
+            0.5, 0, 0,
+            0.25, 0, 0.5
     };
+
 
     unsigned int VBO, VAO;
     glGenVertexArrays(1, &VAO);
@@ -146,7 +153,7 @@ int main()
         // draw our first triangle
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
-        glDrawArrays(GL_TRIANGLES, 0, 6);
+        glDrawArrays(GL_TRIANGLES, 0, 12);
         // glBindVertexArray(0); // no need to unbind it every time
 
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
